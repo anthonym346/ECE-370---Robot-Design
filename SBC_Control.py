ca18549f20233c1f176c69ffad60152484261f2d
anthonym346 <46006659+anthonym346@users.noreply.github.com>
2019-04-17 09:45:14 -0400

    Add files via upload

---
+++ SBC_Control.py
@@ -0,0 +1,42 @@
+import socket
+
+global X
+
+def Send(Message): #send string through udp
+	UDP_IP = "127.0.0.1" #Replace with feather AP IP
+	UDP_PORT = 5005
+	#MESSAGE = "(10,0,1)" #(vel_d,theta_d,mode) #mode: 0=reset,1=normal
+
+	print "UDP target IP:", UDP_IP
+	print "UDP target port:", UDP_PORT
+	print "message:", MESSAGE
+
+	sock = socket.socket(socket.AF_INET, # Internet
+						 socket.SOCK_DGRAM) # UDP
+	sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+def Connect2Port(): #Connect to feather AP
+
+	return
+	
+def Init_Struct(X): #Initialize struct to send instructions
+	return
+	
+def parseInput(input): # get vel, theta, and mode from input
+	
+	
+def main():
+    while 1:
+	
+		# get keyboard input, waits until enter pressed
+		input = raw_input(">> ")
+		
+		v,theta,mode = parseInput(input)
+		
+		X = "(%f,%f,%d)" % (v, theta, mode)"
+		
+		Send(X)
+
+
+if __name__ == '__main__':
+    main()
